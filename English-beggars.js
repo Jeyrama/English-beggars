5a4fdbb5afb0136103faea9ba306f77c87da292b
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-12-12 17:14:38 -0800

    Update English-beggars.js

--- English-beggars.js
+++ English-beggars.js
@@ -33,3 +33,11 @@ function beggars(values, n) {
 }
 
 // or
+
+function beggars(values, n) {
+  let take = Array(n).fill(0);
+  for (let i = 0; i < values.length; i++) {
+    take[i % n] += values[i];
+  }
+  return take;
+}
